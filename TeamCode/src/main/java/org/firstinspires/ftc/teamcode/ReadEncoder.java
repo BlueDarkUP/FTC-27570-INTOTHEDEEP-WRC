4297d6118d35bf9d92e64c2acdac3cd31849008f
BlueDarkUP <119296966+BlueDarkUP@users.noreply.github.com>
2025-06-02 11:30:56 +0800

    更改文件位置

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ReadEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ReadEncoder.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package pedroPathing;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
