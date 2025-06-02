260dfdc68e7f8c9d98d7cf07425eb67cffcb7f2f
LucaLmz <qumingquchangbuhuichong@outlook.com>
2025-06-02 11:22:02 +0800

    燥称冯福

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoCode/SpecAutoBY27570.java
@@ -0,0 +1,253 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.AutoCode;
+
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Point;
+import com.pedropathing.util.Constants;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.ConstantMap;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.AlgorithmLibrary;
+
+/**
+ * This is the fucking best autonomous code in the world.
+ * @author Luca Li
+ * @version 2025/5
+ */
+
+@Autonomous(name = "Auto_Spec_7+Park_27570", group = "Competition")
+public class SpecAutoBY27570 extends OpMode{
+    private Follower follower;
+    private AlgorithmLibrary Algorithm;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+
+    /** This is the variable where we store the state of our auto.
+     * It is used by the pathUpdate method. */
+    private int pathState;
+    private final Pose startPose = new Pose(8.955, 63, Math.toRadians(0));
+    private final Pose GP1= new Pose(50,35,Math.toRadians(0));
+    private final Pose RP1 = new Pose(50,23,Math.toRadians(0));
+    //Control point between GP1 and RP1
+    private final Pose RP1C1 = new Pose(64.7,37,Math.toRadians(9));
+    private final Pose RP1C2 = new Pose(61.8,22.5,Math.toRadians(0));
+    private final Pose Push1 = new Pose(20,23,Math.toRadians(0));
+    private final Pose RP2 = new Pose(50,14,Math.toRadians(0));
+    //Control point between RP1 and RP2
+    private final Pose RP2C1 = new Pose(64.7,22.9,Math.toRadians(0));
+    private final Pose RP2C2 = new Pose(60.4,13.1,Math.toRadians(0));
+    private final Pose Push2 = new Pose(20,14,Math.toRadians(0));
+
+
+    private final Pose[] scorePose  = {
+            new Pose(39, 76, Math.toRadians(0)),
+            new Pose(39, 73, Math.toRadians(0)),
+            new Pose(39, 71, Math.toRadians(0)),
+            new Pose(39, 69, Math.toRadians(0))
+    };
+
+
+    private final Pose GetSpecPosition = new Pose(8.955,31,Math.toRadians(0));
+    private final Pose GSControlP = new Pose(20.8,32.7);
+    private final Pose parkPose = new Pose(10, 10, Math.toRadians(0));
+
+    private int Specnum = 0;
+
+    private Path scorePreload, park;
+    private PathChain Push;
+    private PathChain[] Scoring;
+    private PathChain[] GetSpec;
+    public void buildPaths() {
+
+        /* This is our scorePreload path. We are using a BezierLine, which is a straight line. */
+        scorePreload = new Path(new BezierLine(new Point(startPose), new Point(scorePose[0])));
+        scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose[0].getHeading());
+        Push = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(GetSpecPosition),new Point(GP1)))
+                .setLinearHeadingInterpolation(GetSpecPosition.getHeading(),GP1.getHeading())
+
+                .addPath(new BezierCurve(new Point(GP1),new Point(RP1C1),new Point(RP1C2),new Point(RP1)))
+                .setLinearHeadingInterpolation(GP1.getHeading(),RP1.getHeading())
+
+                .addPath(new BezierLine(new Point(RP1),new Point(Push1)))
+                .setLinearHeadingInterpolation(RP1.getHeading(),Push1.getHeading())
+
+                .addPath(new BezierLine(new Point(Push1),new Point(RP1)))
+                .setLinearHeadingInterpolation(Push1.getHeading(),RP1.getHeading())
+
+                .addPath(new BezierCurve(new Point(RP1),new Point(RP2C1),new Point(RP2C2),new Point(RP2)))
+                .setLinearHeadingInterpolation(RP1.getHeading(),RP2.getHeading())
+
+                .addPath(new BezierLine(new Point(RP2),new Point(Push2)))
+                .setLinearHeadingInterpolation(RP2.getHeading(),Push2.getHeading())
+
+                .addPath(new BezierCurve(new Point(Push2),new Point(GSControlP),new Point(GetSpecPosition)))
+                .setLinearHeadingInterpolation(Push2.getHeading(),GetSpecPosition.getHeading())
+                .build();
+        for(int i = 0; i < 3; i++){
+            GetSpec[i] = follower.pathBuilder()
+                    .addPath(new BezierLine(new Point(scorePose[i]),new Point(GetSpecPosition)))
+                    .setLinearHeadingInterpolation(scorePose[i].getHeading(),GetSpecPosition.getHeading())
+                    .build();
+        }
+        for(int i = 0; i < 2; i++){
+            Scoring[i] = follower.pathBuilder()
+                    .addPath(new BezierLine(new Point(GetSpecPosition),new Point(scorePose[i+1])))
+                    .setLinearHeadingInterpolation(GetSpecPosition.getHeading(),scorePose[i+1].getHeading())
+                    .build();
+        }
+
+        park = new Path(new BezierCurve(new Point(scorePose[3]),new Point(parkPose)));
+        park.setLinearHeadingInterpolation(scorePose[3].getHeading(), parkPose.getHeading());
+    }
+
+    /** This switch is called continuously and runs the pathing, at certain points, it triggers the action state.
+     * Everytime the switch changes case, it will reset the timer. (This is because of the setPathState() method)
+     * The followPath() function sets the follower to run the specific path, but does NOT wait for it to finish before moving on. */
+    public void autonomousPathUpdate () throws InterruptedException {
+        switch (pathState) {
+            case 0:
+                follower.followPath(scorePreload,true);
+                follower.update();
+                Algorithm.ArmController("Up");
+                setPathState(1);
+                break;
+            case 1:
+                if(!follower.isBusy()){
+                    Algorithm.BackGrabAction(ConstantMap.BackGrab_Initialize);
+                    Specnum++;
+                    follower.followPath(GetSpec[Specnum-1],true);
+                    follower.update();
+                    Algorithm.SlideController("Back");
+                    Thread.sleep(ConstantMap.SleepMSAfterScoring);
+                    Algorithm.ArmController("Down");
+                    if(Specnum<3) {
+                        setPathState(2);
+                    }
+                    setPathState(3);
+                    break;
+                }
+            case 2:
+                if(!follower.isBusy()){
+                    Algorithm.ForwardGrabController("Open");
+                    Algorithm.BackGrabAction(ConstantMap.BackGrab_TightPosition);
+                    follower.followPath(Scoring[Specnum-1],true);
+                    follower.update();
+                    Algorithm.ArmController("Up");
+                    setPathState(1);
+                    break;
+                }
+            case 3:
+                if(!follower.isBusy()){
+                    follower.followPath(Push);
+                    follower.update();
+                    setPathState(5);
+                }
+            case 4:
+                if(!follower.isBusy()) {
+                    Algorithm.BackGrabAction(ConstantMap.BackGrab_Initialize);
+                    follower.followPath(GetSpec[2],true);
+                    follower.update();
+                    Thread.sleep(ConstantMap.SleepMSAfterScoring);
+                    Algorithm.ArmController("Down");
+                    setPathState(5);
+                    break;
+                }
+            case 5:
+                if(!follower.isBusy()){
+                    Algorithm.BackGrabAction(ConstantMap.BackGrab_TightPosition);
+                    follower.followPath(Scoring[2],true);
+                    follower.update();
+                    Algorithm.ArmController("Up");
+                    Specnum++;
+                    if(Specnum<7) {
+                        setPathState(4);
+                        break;
+                    }
+                    setPathState(6);
+                    break;
+                }
+            case 6:
+                if(!follower.isBusy()) {
+                    Algorithm.BackGrabAction(ConstantMap.BackGrab_Initialize);
+                    follower.followPath(park,false);
+                    follower.update();
+                    Thread.sleep(ConstantMap.SleepMSAfterScoring);
+                    Algorithm.ArmController("Down");
+                    setPathState(-1);
+                    break;
+                }
+        }
+    }
+
+    /** These change the states of the paths and actions
+     * It will also reset the timers of the individual switches **/
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
+    @Override
+    public void loop() {
+
+        // These loop the movements of the robot
+        follower.update();
+        try {
+            autonomousPathUpdate();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Feedback to Driver Hub
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
+
+    /** This method is called once at the init of the OpMode. **/
+    @Override
+    public void init() {
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+        Algorithm = new AlgorithmLibrary(hardwareMap);
+        Algorithm.Initialize_All_For_Autonomous();
+
+        Constants.setConstants(FConstants.class, LConstants.class);
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
+        buildPaths();
+    }
+
+    /** This method is called continuously after Init while waiting for "play". **/
+    @Override
+    public void init_loop() {}
+
+    /** This method is called once at the start of the OpMode.
+     * It runs all the setup actions, including building paths and starting the path system **/
+    @Override
+    public void start() {
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+
+    /** We do not use this because everything should automatically disable **/
+    @Override
+    public void stop() {
+    }
+}
+
+/**
+ * To my lover jsy
+ */
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/AlgorithmLibrary.java
@@ -0,0 +1,214 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.constants;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.Objects;
+
+/**
+ * 优雅永不过时
+ * @author LucaLi
+ * @version 2025/5
+ */
+public class AlgorithmLibrary {
+
+    public ElapsedTime EmergencyInitTimer = new ElapsedTime();
+    public static DcMotorEx Left_Hanging_Motor = null;
+    public static DcMotorEx Right_Hanging_Motor = null;
+    public static DcMotorEx BigArm = null;
+    public static Servo BackArm,back_grab,forward_claw,intake_rotate,claw_heng,arm_forward,forward_slide = null,intake_spinner;
+    private int MotorLastPosition;
+
+
+    public AlgorithmLibrary(HardwareMap hardwareMap){
+        //Get hardware
+        Left_Hanging_Motor = hardwareMap.get(DcMotorEx.class, "LeftHangingMotor");
+        Right_Hanging_Motor = hardwareMap.get(DcMotorEx.class, "RightHangingMotor");
+        BigArm = hardwareMap.get(DcMotorEx.class,"big_arm");
+        arm_forward = hardwareMap.get(Servo.class,"arm_forward");
+        forward_slide = hardwareMap.get(Servo.class,"forward_slide");
+        BackArm = hardwareMap.get(Servo.class,"back_arm");
+        forward_claw =hardwareMap.get(Servo.class,"forward_claw");
+        intake_rotate= hardwareMap.get(Servo.class,"intake_rotate");
+        back_grab = hardwareMap.get(Servo.class,"backgrab");
+        intake_spinner = hardwareMap.get(Servo.class,"rotate_platform");
+
+        //Set motor direction
+        Left_Hanging_Motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        Right_Hanging_Motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        BigArm.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+
+    }
+    public void InitializeLift(){
+        Left_Hanging_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Right_Hanging_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        Left_Hanging_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Right_Hanging_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        Left_Hanging_Motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Right_Hanging_Motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void InitializeArm(){
+        BigArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BigArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BigArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+    public void Initialize_All_For_Autonomous(){
+
+        //Initialize motor
+        InitializeLift();
+        InitializeArm();
+        //Set servo position
+        back_grab.setPosition(ConstantMap.BackGrab_TightPosition);
+        BackArm.setPosition(ConstantMap.BACK_ARM_INITIALIZE_POSITION);
+        arm_forward.setPosition(ConstantMap.Arm_Forward_Initialize_Position);
+        forward_slide.setPosition(ConstantMap.Slide_In_Position);
+        intake_spinner.setPosition(ConstantMap.Intake_spinner_Initial_Position);
+    }
+    public void Initialize_All_For_TeleOp() throws InterruptedException {
+        //Initialize motor
+        Left_Hanging_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Right_Hanging_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BigArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Left_Hanging_Motor.setPower(-0.7);
+        Right_Hanging_Motor.setPower(-0.7);
+        BigArm.setPower(-0.4);
+        Thread.sleep(100);
+        Left_Hanging_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Right_Hanging_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BigArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Left_Hanging_Motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Right_Hanging_Motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BigArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //Set servo position
+        back_grab.setPosition(ConstantMap.BackGrab_Initialize);
+        BackArm.setPosition(ConstantMap.BACK_ARM_INITIALIZE_POSITION);
+        arm_forward.setPosition(ConstantMap.Arm_Forward_Initialize_Position);
+        forward_slide.setPosition(ConstantMap.Slide_In_Position);
+        forward_claw.setPosition(ConstantMap.ForwardClaw_Initialize_Position);
+        intake_rotate.setPosition(ConstantMap.Intake_rotate_Initial_Position);
+        intake_spinner.setPosition(ConstantMap.Intake_spinner_Initial_Position);
+    }
+    public void ArmController(String flag){
+        if(Objects.equals(flag,"Up")){
+            LiftAction(ConstantMap.Lift_Up_HighChamber_Position);
+            BigArmMotorAction(ConstantMap.Big_Arm_Set_Position);
+            BackArmAction(ConstantMap.BACK_ARM_SET_POSITION);
+            return;
+        }
+        BigArmMotorAction(ConstantMap.Big_Arm_Reset_Position);
+        BackArmAction(ConstantMap.BACK_ARM_RESET_POSITION);
+        LiftAction(ConstantMap.Lift_Down_Position);
+    }
+
+    public void IntakeController(String flag) throws InterruptedException {
+        if(Objects.equals(flag,"Take")) {
+            arm_forward.setPosition(ConstantMap.Arm_Forward_Down_Position);
+            Thread.sleep(60);
+            ForwardGrabController("Close");
+            Thread.sleep(120);
+            arm_forward.setPosition(ConstantMap.Arm_Forward_Up_Position);
+            return;
+        }
+        ForwardGrabController("Open");
+    }
+    public void SpinnerController(String flag){
+        if(Objects.equals(flag,"Put down")){
+            intake_spinner.setPosition(ConstantMap.Intake_spinner_PutDown_Position);
+            return;
+        }
+        intake_spinner.setPosition(ConstantMap.Intake_spinner_Initial_Position);
+    }
+    public void ForwardGrabController(String flag){
+        if(Objects.equals(flag, "Open")){
+            forward_claw.setPosition(ConstantMap.ForwardClaw_Initialize_Position);
+            return;
+        }
+        forward_claw.setPosition(ConstantMap.ForwardClaw_Tight_Position);
+    }
+    public void RotateController(String flag){
+        if(Objects.equals(flag,"Turn")) {
+            intake_rotate.setPosition(ConstantMap.Intake_rotate_Turned_Position);
+            return;
+        }
+        intake_rotate.setPosition(ConstantMap.Intake_rotate_Initial_Position);
+    }
+    public void BackGrabAction(double Position){
+        back_grab.setPosition(Position);
+    }
+    public void SlideController(String flag){
+        if(Objects.equals(flag,"Out")){
+            intake_spinner.setPosition(ConstantMap.Intake_spinner_Initial_Position);
+            arm_forward.setPosition(ConstantMap.Arm_Forward_Up_Position);
+            forward_slide.setPosition(ConstantMap.Slide_Out_Position);
+            return;
+        }
+        arm_forward.setPosition(ConstantMap.Arm_Forward_Initialize_Position);
+        forward_slide.setPosition(ConstantMap.Slide_In_Position);
+        intake_spinner.setPosition(ConstantMap.Intake_spinner_PutDown_Position);
+    }
+    public void EmergencyMotorPowerSetting(){
+        BigArm.setPower(-0.5);
+        Left_Hanging_Motor.setPower(-0.7);
+        Right_Hanging_Motor.setPower(-0.7);
+    }
+    public boolean IsTop(DcMotorEx Motor){
+        if(Motor.getCurrentPosition()<MotorLastPosition+3||Motor.getCurrentPosition()>MotorLastPosition-3){
+            MotorLastPosition = Motor.getCurrentPosition();
+            return true;
+        }
+        MotorLastPosition = Motor.getCurrentPosition();
+        return false;
+    }
+
+    public void ClimbController(){
+        LiftAction(ConstantMap.Lift_Up_Climb_Position);
+        while(true){
+            if(!Left_Hanging_Motor.isBusy()&&!Right_Hanging_Motor.isBusy()){
+                Left_Hanging_Motor.setPower(-1);
+                Right_Hanging_Motor.setPower(-1);
+            }
+        }
+    }
+
+    private void LiftAction(int Position){
+        //Set target position
+        Left_Hanging_Motor.setTargetPosition(Position);
+        Right_Hanging_Motor.setTargetPosition(Position);
+
+        //Set mode
+        Left_Hanging_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Right_Hanging_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //Set Power
+        Left_Hanging_Motor.setPower(1);
+        Right_Hanging_Motor.setPower(1);
+    }
+    private void BackArmAction(double Position){
+        BackArm.setPosition(Position);
+    }
+    private void BigArmMotorAction(int Position){
+        //Set Target Position
+        BigArm.setTargetPosition(Position);
+
+        //Set Mode
+        BigArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if(Position == ConstantMap.Big_Arm_Set_Position){
+            BigArm.setPower(ConstantMap.Big_Arm_Up_Power);
+            return;
+        }
+        //Set Power
+        BigArm.setPower(ConstantMap.Big_Arm_Down_Power);
+    }
+}
