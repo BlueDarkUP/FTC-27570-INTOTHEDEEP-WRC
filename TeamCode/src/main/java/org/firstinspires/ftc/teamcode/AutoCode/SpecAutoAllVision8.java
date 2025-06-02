823f626e20e0e4ecd02d237f3aafde4d190d7fba
BlueDarkUP <119296966+BlueDarkUP@users.noreply.github.com>
2025-06-02 10:53:16 +0800

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode/SpecAutoAllVision8.java
@@ -0,0 +1,205 @@
+package pedroPathing.AutoCode;
+
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Point;
+import com.pedropathing.util.Constants;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.opencv.core.Algorithm;
+
+import pedroPathing.constants.FConstants;
+import pedroPathing.constants.LConstants;
+import pedroPathing.constants.ConstantMap;
+import pedroPathing.constants.AlgorithmLibrary;
+
+/**
+ * This is the fucking best autonomous code in the world.
+ * @author Luca Li
+ * @version 2025/5
+ */
+
+@Autonomous(name = "Auto_Spec_8+Park_27570", group = "Competition")
+public class SpecAutoAllVision8 extends OpMode{
+    private Follower follower;
+    private AlgorithmLibrary Algorithm;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+
+    /** This is the variable where we store the state of our auto.
+     * It is used by the pathUpdate method. */
+    private int pathState;
+
+    private final Pose startPose = new Pose(8.955, 63, Math.toRadians(0));
+    private final Pose[] scorePose  = {
+            new Pose(39, 76.5, Math.toRadians(0)),
+            new Pose(39, 75.2, Math.toRadians(0)),
+            new Pose(39, 73.8, Math.toRadians(0)),
+            new Pose(39, 72.7, Math.toRadians(0)),
+            new Pose(39, 71.5, Math.toRadians(0)),
+            new Pose(39, 70, Math.toRadians(0)),
+            new Pose(39, 68.5, Math.toRadians(0)),
+            new Pose(39, 67, Math.toRadians(0))
+    };
+
+
+    private final Pose GetSpecPosition = new Pose(8.955,31,Math.toRadians(0));
+    private final Pose parkPose = new Pose(10, 10, Math.toRadians(0));
+
+    private int Specnum = 1;
+
+    private Path scorePreload, park;
+    private PathChain[] Scoring;
+    private PathChain[] GetSpec;
+    public void buildPaths() {
+
+        /* This is our scorePreload path. We are using a BezierLine, which is a straight line. */
+        scorePreload = new Path(new BezierLine(new Point(startPose), new Point(scorePose[0])));
+        scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose[0].getHeading());
+
+        for(int i = 0; i < 6; i++){
+            GetSpec[i] = follower.pathBuilder()
+                    .addPath(new BezierLine(new Point(scorePose[i]),new Point(GetSpecPosition)))
+                    .setLinearHeadingInterpolation(scorePose[i].getHeading(),GetSpecPosition.getHeading())
+                    .build();
+        }
+        for(int i = 0; i < 7; i++){
+            Scoring[i] = follower.pathBuilder()
+                    .addPath(new BezierLine(new Point(GetSpecPosition),new Point(scorePose[i+1])))
+                    .setLinearHeadingInterpolation(GetSpecPosition.getHeading(),scorePose[i+1].getHeading())
+                    .build();
+        }
+
+        park = new Path(new BezierCurve(new Point(scorePose[7]),new Point(parkPose)));
+        park.setLinearHeadingInterpolation(scorePose[7].getHeading(), parkPose.getHeading());
+    }
+
+    /** This switch is called continuously and runs the pathing, at certain points, it triggers the action state.
+     * Everytime the switch changes case, it will reset the timer. (This is because of the setPathState() method)
+     * The followPath() function sets the follower to run the specific path, but does NOT wait for it to finish before moving on. */
+    public void autonomousPathUpdate () throws InterruptedException {
+        switch (pathState) {
+            case 0:
+                follower.followPath(scorePreload,true);
+                follower.update();
+                Algorithm.ArmController("Up");
+                // Thread.sleep(200);
+
+                //Algorithm.BackGrabAction(ConstantMap.BackGrab_LaxPosition);
+                setPathState(2);
+                break;
+            /*case 1:
+                if(!follower.isBusy()){
+                    follower.followPath(scorePreload,false);
+                    follower.update();
+                    setPathState(2);
+                    break;
+                }*/
+            case 2:
+                if(!follower.isBusy()) {
+                    Algorithm.BackGrabAction(ConstantMap.BackGrab_Initialize);
+                    follower.followPath(GetSpec[Specnum-1],true);
+                    follower.update();
+                    Algorithm.SlideController("Back");
+                    Thread.sleep(ConstantMap.SleepMSAfterScoring);
+                    Algorithm.ArmController("Down");
+                    setPathState(3);
+                    break;
+                }
+            case 3:
+                if(!follower.isBusy()){
+                    Algorithm.ForwardGrabController("Open");
+                    Algorithm.BackGrabAction(ConstantMap.BackGrab_TightPosition);
+                    follower.followPath(Scoring[Specnum-1],true);
+                    follower.update();
+                    Algorithm.ArmController("Up");
+                    Specnum++;
+                    if(Specnum<8) {
+                        setPathState(2);
+                        break;
+                    }
+                    setPathState(4);
+                    break;
+                }
+            case 4:
+                if(!follower.isBusy()) {
+                    Algorithm.BackGrabAction(ConstantMap.BackGrab_Initialize);
+                    follower.followPath(park,false);
+                    follower.update();
+                    Thread.sleep(ConstantMap.SleepMSAfterScoring);
+                    Algorithm.ArmController("Down");
+                    setPathState(-1);
+                    break;
+                }
+        }
+    }
+
+    /** These change the states of the paths and actions
+     * It will also reset the timers of the individual switches **/
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
+    @Override
+    public void loop() {
+
+        // These loop the movements of the robot
+        follower.update();
+        try {
+            autonomousPathUpdate();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Feedback to Driver Hub
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
+
+    /** This method is called once at the init of the OpMode. **/
+    @Override
+    public void init() {
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+        Algorithm = new AlgorithmLibrary(hardwareMap);
+        Algorithm.Initialize_All_For_Autonomous();
+
+        Constants.setConstants(FConstants.class, LConstants.class);
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
+        buildPaths();
+    }
+
+    /** This method is called continuously after Init while waiting for "play". **/
+    @Override
+    public void init_loop() {}
+
+    /** This method is called once at the start of the OpMode.
+     * It runs all the setup actions, including building paths and starting the path system **/
+    @Override
+    public void start() {
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+
+    /** We do not use this because everything should automatically disable **/
+    @Override
+    public void stop() {
+    }
+}
+
+/**
+ * To my lover jsy
+ */
